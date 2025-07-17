cfde9116b50b5173a5a16bc2cf46e86ecb4aea6b
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2025-07-17 17:01:49 +0300

    trasaction_fees, interest rate

--- controller/loan/getTrasactionFees.ts
+++ controller/loan/getTrasactionFees.ts
@@ -1,12 +1,11 @@
-
-import { Request, Response, NextFunction } from 'express';
 import { PrismaClient } from '@prisma/client';
-
-
 
 const prisma = new PrismaClient();
 
-export const getTransactionFee = async (amount: number, tenantId: number): Promise<number> => {
+export const getTransactionFee = async (
+  amount: number,
+  tenantId: number
+): Promise<number> => {
   const band = await prisma.transactionCostBand.findFirst({
     where: {
       tenantId,
